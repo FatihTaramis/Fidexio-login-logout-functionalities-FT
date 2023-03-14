7597057498393e875e73165657eeba07259d91d5
Fatih Taramis <fatihtaramis@hotmail.com>
2023-03-14 17:43:07 +0000

    updated stepdefinitions

--- src/test/java/com/cydeo/step_definitions/LoginFunctionalityStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/LoginFunctionalityStepDefinitions.java
@@ -4,6 +4,8 @@ import com.cydeo.pages.FidexioLoginPage;
 import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.*;
+import io.cucumber.messages.types.Examples;
+import io.cucumber.plugin.event.Node;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -32,24 +34,42 @@ public class LoginFunctionalityStepDefinitions {
     }
 
 
-    @Then("User clicks the login button")
+    @And("User clicks the login button")
     public void userClicksTheLoginButton() {
         fidexioLoginPage.loginButton.click();
     }
 
     @Then("User should successfully log in to the homepage")
     public void userShouldSuccessfullyLogInToTheHomepage() {
-
+        BrowserUtils.sleep(5);
         BrowserUtils.verifyTitle("#Inbox - Odoo");
 
     }
 
 
-    @Then("User should see the message")
-    public void userShouldSeeTheMessage() {
-        
+    @Then("User should see the {string} message")
+    public void userShouldSeeTheMessage(String arg0) {
+
+        if (fidexioLoginPage.emailBox.getAttribute("value").isEmpty()){
+            System.out.println("fidexioLoginPage.emailBox.getText() = " + fidexioLoginPage.emailBox.getText());
+            Assert.assertEquals("Please fill in this field.", fidexioLoginPage.emailBox.getAttribute("validationMessage"));
+        }
+
+        if (fidexioLoginPage.passwordBox.getAttribute("value").isEmpty()){
+            System.out.println("fidexioLoginPage.passwordBox.getText() = " + fidexioLoginPage.passwordBox.getText());
+            Assert.assertEquals("Please fill in this field.", fidexioLoginPage.passwordBox.getAttribute("validationMessage"));
+        }
+
+    }
+
+
+    @Then("The User should see the message {string}")
+    public void theUserShouldSeeTheMessage(String arg0) {
+
+        Assert.assertTrue(fidexioLoginPage.wrongLoginPasswordDisplay.isDisplayed());
     }
 }
+
 
 //    @When("User enters the correct SalesManager credentials")
 //    public void userEntersTheCorrectSalesManagerCredentials() {
